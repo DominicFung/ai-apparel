0152a1070eee9f1f8d817aa8e0b99dc5aeed93b3
Dom Fung <fung_dominic@hotmail.com>
2022-11-20 10:51:42 -0500

    loading button

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,9 +20,6 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import Head from 'next/head'
 
-// To return to non-web
-// aws amplify update-app --region us-east-1 --app-id d13h2md0ftccbx --platform WEB_DYNAMIC
-
 const NUM_IMAGES = 3
 
 const showCase = [
@@ -54,11 +51,13 @@ const Home: NextPageWithLayout = () => {
     }
   ])
   const [generateResult, setGenerateResult] = useState<ReplicateSDResponse[]>([])
+  const [ loading, setLoading ] = useState(false)
 
   const [ openProducts, setOpenProducts ] = useState(false)
   const [ activeItemId, setActiveItemId ] = useState<string>("")
 
   let generateImages = async () => {
+    setLoading(true)
     let url = '/api/userid/replicate/stablediffusion/generate'
     let response = await (await fetch(url, {
       method: 'POST', 
@@ -84,6 +83,12 @@ const Home: NextPageWithLayout = () => {
     } else {
       images[index] = response
       setImages([...images])
+
+      let isLoading = false
+      for (let i of images) {
+        if (i.status !== 'COMPLETE'){ isLoading=true; break }
+      }
+      setLoading(isLoading)
     }
   }
 
@@ -164,9 +169,21 @@ const Home: NextPageWithLayout = () => {
             
             <div className='flex w-full flex-row-reverse lg:mt-10'>
               <button onClick={generateImages}
-                className={`${s.actionButton} hidden lg:flex flex-row py-5 px-3 text-white rounded hover:bg-gray-700 hover:text-white active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none disabled:bg-gray-400`}
-              > <span className='px-1'>Generate</span>
-                <BoltIcon className={`${s.actionIcon} h-6 w-6 inline-block text-yellow-200`} />
+                className={`${s.actionButton} hidden lg:flex flex-row py-5 px-3 text-white rounded hover:bg-gray-700 hover:text-white active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none`}
+                disabled={loading}
+              > 
+                { loading && 
+                <div role="status" className='h-0 w-full flex justify-center'>
+                  <svg className="inline mr-2 w-6 h-6 text-gray-700 animate-spin fill-pink-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div> }
+                <span className='flex flex-row'>
+                  <span className='px-1'>Generate</span>
+                  <BoltIcon className={`${s.actionIcon} h-6 w-6 inline-block ${ loading ? "text-gray-200" : "text-yellow-200" }`} />
+                </span>
               </button>
 
               <input
@@ -205,10 +222,21 @@ const Home: NextPageWithLayout = () => {
             </div>
             <div className='flex lg:hidden w-full justify-center'>
               <button onClick={generateImages}
-                  className={`${s.actionButton} justify-center m-1 py-3 px-3 text-white rounded hover:bg-gray-700 hover:text-white active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none disabled:bg-gray-400`}
-              > <span className='flex flex-row'>
+                  className={`${s.actionButton} justify-center m-1 py-3 px-3 text-white rounded active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none`}
+                  disabled={loading}
+              > 
+                { loading && 
+                <div role="status" className='h-0'>
+                  <svg className="inline mr-2 w-6 h-6 text-gray-700 animate-spin fill-pink-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>}
+              
+                <span className='flex flex-row'>
                   <span className='px-1'>Generate</span>
-                  <BoltIcon className={`${s.actionIcon} h-6 w-6 inline-block text-yellow-200`} />
+                  <BoltIcon className={`${s.actionIcon} h-6 w-6 inline-block ${ loading ? "text-gray-200" : "text-yellow-200" }`} />
                 </span>
               </button>
             </div>
